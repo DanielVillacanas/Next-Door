c1b8c7b8c1f834d0810571981ef6a6ff359a23ce
Danivilla <dasvilla95@gmail.com>
2021-12-06 11:28:47 +0100

    test

--- server/routes/products/allProducts.routes.js
+++ server/routes/products/allProducts.routes.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const Product = require("../../models/Product.model");
-
+const User = require("../../models/User.model");
 router.get("/", (req, res, next) => {
-  //LLamo a la BBDD y obtengo todos los productos los mando res.JSON
   Product.find()
     .then((response) => res.json(response))
     .catch((err) => console.log(err));
 });
-router.post(
-  "/:id",
+router.put(
+  "/allProducts/cart/add/:id",
+  /*AQUI IRIA UN MIDDLEWARE*/ (req, res) => {
+    const { id } = req.params;
+    const user = req.session.currentUser;
+    User.findOneAndUpdate(user._id, { $push: { productsCart: id } }).then((response) =>
+      res.json(response)
+    );
+  }
+);
+router.get(
+  "/details/:id",
   /*AQUI IRIA UN MIDDLEWARE*/ (req, res) => {
     //Llamo a la BBDD y lo pusheo a la lista de productos del carrito
   }
@@ -16,4 +25,5 @@ router.post(
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
 
+router.get();
 module.exports = router;
