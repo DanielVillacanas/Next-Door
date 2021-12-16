6d463149d37369b25dd903f15db0dc391d4f781f
Danivilla <dasvilla95@gmail.com>
2021-12-16 10:57:48 +0100

    deploy

--- server/app.js
+++ server/app.js
@@ -2,16 +2,19 @@ require("dotenv/config");
 
 require("./db");
 
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 require("./config")(app);
 require("./config/session.config")(app);
 
 require("./routes")(app);
 
-app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 module.exports = app;
