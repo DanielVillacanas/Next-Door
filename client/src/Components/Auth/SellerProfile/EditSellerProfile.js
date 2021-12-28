5ea9e9ebb83c3ae96302a1533348e99b3cd2f110
Danivilla <dasvilla95@gmail.com>
2021-12-28 12:27:35 +0100

    loading spinner

--- client/src/Components/Auth/SellerProfile/EditSellerProfile.js
+++ client/src/Components/Auth/SellerProfile/EditSellerProfile.js
@@ -16,6 +16,9 @@ export default function EditSellerProfile(props) {
     address: props.seller?.address,
     img_url: "",
   });
+  const [loadingImg, setLoadingImg] = useState(false);
+  const [errMessage, setError] = useState(undefined);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     !errMessage &&
@@ -27,7 +30,6 @@ export default function EditSellerProfile(props) {
         })
         .catch((err) => console.log(err));
   };
-  const [errMessage, setError] = useState(undefined);
 
   const handleInputChange = (e) => {
     const { name, value } = e.currentTarget;
@@ -45,11 +47,13 @@ export default function EditSellerProfile(props) {
   };
 
   const handleUploadChange = (e) => {
+    setLoadingImg(true);
     const uploadData = new FormData();
     uploadData.append("imageData", e.target.files[0]);
     uploadService
       .uploadImage(uploadData)
       .then((response) => {
+        setLoadingImg(false);
         setSeller({ ...seller, img_url: response.data.cloudinary_url });
       })
       .catch((err) => console.log("El error", { err }));
@@ -184,10 +188,40 @@ export default function EditSellerProfile(props) {
                               d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                             />
                           </svg>
-
-                          <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
-                            Sube una foto
-                          </p>
+                          {!loadingImg && (
+                            <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                              Sube una foto
+                            </p>
+                          )}
+                          {loadingImg && (
+                            <button
+                              type="button"
+                              class="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-green-500 hover:bg-green-400 transition ease-in-out duration-150 cursor-not-allowed"
+                              disabled=""
+                            >
+                              <svg
+                                class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                              >
+                                <circle
+                                  class="opacity-25"
+                                  cx="12"
+                                  cy="12"
+                                  r="10"
+                                  stroke="currentColor"
+                                  stroke-width="4"
+                                ></circle>
+                                <path
+                                  class="opacity-75"
+                                  fill="currentColor"
+                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                ></path>
+                              </svg>
+                              Cargando...
+                            </button>
+                          )}
                         </div>
 
                         <input
